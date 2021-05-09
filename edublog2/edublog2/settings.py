8593f65fbf016b6284aaf17a7badb367b2dc966f
Anusha <47149499+anushanagula@users.noreply.github.com>
2021-05-09 22:06:00 +0530

    update1

--- edublog2/edublog2/settings.py
+++ edublog2/edublog2/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,4 +141,3 @@ CKEDITOR_CONFIGS = {
         'toolbar': None,
     },
 }
-django_heroku.settings(locals())
